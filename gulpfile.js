f60b2f0f4a159cd90fc0de1b6aa4288d318b4a24
bugnana <1223646107@qq.com>
2018-04-16 10:41:24 +0800

    提交第二次

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 gulp.task('connect', function() {
     connect.server({
         port: 8989,
         livereload: true
     })
 });
+// gulp.task('uglify', function() {
+//     gulp.src('*.js')
+//         .pipe(gulp.concat())
+//     gulp.dest('/lib')
+// })
 gulp.task('index', function() {
-    gulp.src('.')
+    gulp.src('.', )
         .pipe(connect.reload())
 });
 gulp.task('watch', function() {
-    gulp.watch('index.html', ['index'])
+    gulp.watch('./index.html', './css/*.css', ['index'])
 });
 gulp.task('default', ['connect', 'watch'])
